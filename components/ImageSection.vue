1fcae945de185464511d95fda3f458e1c7f42e1c
chips <chips@metrica.se>
2019-05-11 19:03:36 +0200

    Remove pixels for now

--- components/ImageSection.vue
+++ components/ImageSection.vue
@@ -1,23 +1,7 @@
 <template>
   <div 
     v-if="src" 
-    class="lg:-mx-32 bg-grey relative overflow-hidden">
-    <div class="hidden lg:block absolute pin-l w-32 h-full z-10">
-      <Pixels
-        :w="4"
-        :h="large?28:16"
-        class="absolute w-full"
-        style="top:50%;transform:translateY(-50%);"
-      />
-    </div>
-    <div class="hidden lg:block absolute pin-r w-32 h-full z-10">
-      <Pixels
-        :w="4"
-        :h="large?28:16"
-        class="absolute w-full"
-        style="top:50%;transform:translateY(-50%) scaleX(-1);"
-      />
-    </div>
+    class="bg-grey relative overflow-hidden">
     <VImage 
       :src="src" 
       :alt="alt" 
